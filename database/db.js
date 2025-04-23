021b5075e8a6f46c8e71cc90be94aa78a4871198
dallivi10 <156099942+dallivi10@users.noreply.github.com>
2025-04-23 18:57:50 -0400

    ,

--- database/db.js
+++ database/db.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'postgres', 
+    database: 'servicioComunitario', 
     password: '1234',
     port: 5432,
 });
