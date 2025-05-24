e9c5df0cafb6b8713450483d2f556b4e8fa2ff53
dallivi10 <156099942+dallivi10@users.noreply.github.com>
2025-05-24 10:30:56 -0400

    integracion de mobiliario arreglado

--- server/salonesRoutes.js
+++ server/salonesRoutes.js
@@ -3,13 +3,16 @@ const pool = require('../database/db'); // Asegúrate de que este archivo esté 
 
 
 
+
 const router = express.Router();
 
 // Ruta para obtener la lista de salones
 router.get('/list', async (req, res) => {
     try {
-        const result = await pool.query('SELECT "idSalon", "modulo" FROM "public"."salon"'); // Selecciona solo los campos necesarios
-        res.json(result.rows); // Devuelve los datos como JSON
+        const result = await pool.query('SELECT "idSalon", "capacidad" ,"modulo" FROM "public"."salon"'); // Selecciona solo los campos necesarios
+        console.log('Resultado de la consulta:', result.rows); // Muestra el resultado en la consola
+        res.json(result.rows);
+         // Devuelve los datos como JSON
     } catch (error) {
         console.error('Error al obtener los salones:', error.message, error.stack);
         res.status(500).send(`Error al obtener los salones: ${error.message}`);
